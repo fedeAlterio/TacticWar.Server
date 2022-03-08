4bb22a1eb9d6920fec6e9182cab3997139dbacdf
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2022-03-08 13:15:15 +0100

    Long Polling tests

---
+++ Tests/TacticWar.Rest.Tests/Utils/LongPollingTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TacticWar.Lib.Tests.Attributes;
+using TacticWar.Rest.Utils.LongPolling;
+
+namespace TacticWar.Test.TacticWar.Rest.Tests.Utils
+{
+    public class LongPollingTests
+    {
+        [FactFor(nameof(UpdateQueue<int>.Get))]
+        public async void Should_ProvideConsistentUpdates()
+        {
+            var queue = new UpdateQueue<int>();
+            var numTask1 = queue.Get();
+            var numTask2 = queue.Get();
+            numTask1.IsCompleted.Should().BeFalse();
+            numTask2.IsCompleted.Should().BeFalse();
+            queue.NotifyNew(1);
+            queue.NotifyNew(2);
+            var num1 = await numTask1;
+            var num2 = await numTask1;
+            num1.Should().Be(1);
+            num2.Should().Be(1);
+        }
+    }
+}
