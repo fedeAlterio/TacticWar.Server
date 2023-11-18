bafc0c1a5c8d43ddd0e8789b08fdc9280437961d
fedeAlterio <48481385+fedeAlterio@users.noreply.github.com>
2023-11-18 17:31:00 +0100

    Fix fullscreen

--- Rest/TacticWarRest/ClientApp/src/app/unity-game-loader/unity-game-loader.component.ts
+++ Rest/TacticWarRest/ClientApp/src/app/unity-game-loader/unity-game-loader.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, WritableSignal, signal } from '@angular/core';
+import { from, fromEvent, map, merge, Observable, startWith, Subscription } from "rxjs";
 
 @Component({
   selector: 'unity-game-loader',
   templateUrl: './unity-game-loader.component.html',
   styleUrls: ['./unity-game-loader.component.scss']
 })
-export class UnityGameLoaderComponent implements OnInit {
-  async ngOnInit(): Promise<void>  {
+export class UnityGameLoaderComponent implements OnInit, AfterViewInit {
+
+  width: WritableSignal<number>;
+  height: WritableSignal<number>;
+
+  constructor(){
+    this.width = signal(0);
+    this.height = signal(0);
+  }
+
+  @ViewChild('root')
+  rootDiv: ElementRef = null!;
+
+  async ngOnInit(): Promise<void> {
+    signal(0)
+    var canvas: HTMLElement = document.querySelector("#unity-canvas") || new HTMLElement();
+    await this.loadGame(canvas);
+  }
+
+  ngAfterViewInit(): void {
+      const resizeEvent = fromEvent(window, 'resize');
+      const fullscreenEvent = fromEvent(window, 'fullscreenchange');
+      merge(fullscreenEvent, resizeEvent)
+      .pipe(     
+        map(_ => undefined),
+        startWith(undefined)
+      ).subscribe(_ => {
+        var width = this.rootDiv.nativeElement.offsetWidth;
+        var height = this.rootDiv.nativeElement.offsetHeight - 50;
+        this.width.set(width);
+        this.height.set(height);
+      })
+  }
+
+  async loadGame(canvas: HTMLElement): Promise<void> {
     var buildUrl = "assets/tactic-war-unity/Build";
     var loaderUrl = buildUrl + "/tacticwar.loader.js";
     var config = {
@@ -19,15 +53,10 @@ export class UnityGameLoaderComponent implements OnInit {
       productVersion: "0.1",
     };
 
-    let container = document.querySelector("#unity-container") || new Element();
-    var canvas: HTMLElement = document.querySelector("#unity-canvas") || new HTMLElement();
-    var loadingBar: HTMLElement = document.querySelector("#unity-loading-bar") || new HTMLElement();
-    var progressBarFull: HTMLElement = document.querySelector("#unity-progress-bar-full") || new HTMLElement();
     var fullscreenButton: HTMLElement = document.querySelector("#unity-fullscreen-button") || new HTMLElement();
     const unityInstance = await createUnityInstance(canvas, config);
     fullscreenButton.onclick = () => {
       unityInstance.SetFullscreen(1);
-    };  
+    };
   }
-
 }
